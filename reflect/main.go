ad7224cf9e31f42de6360872c8fd3bb88b8849f1
wulinlw@gmail.com <wulinlw@gmail.com>
2016-03-25 18:03:11 +0800

    proxy 2.0

--- reflect/main.go
+++ reflect/main.go
@@ -4,77 +4,166 @@ import (
 	"fmt"
 
 	sc "../spacecraft"
-	//"golang.org/x/net/context"
+
+	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"log"
 	"reflect"
+
+	//	"io/ioutil"
+
+	//	"path/filepath"
+	"errors"
+	"net/http"
+
+	"strconv"
 	"time"
 )
 
 func main() {
-	conn, err := grpc.Dial("127.0.0.1:50051", grpc.WithInsecure(), grpc.WithTimeout(time.Second*5), grpc.WithBlock())
+	http.HandleFunc("/index", index)
+	http.ListenAndServe(":8000", nil)
+}
+func index(w http.ResponseWriter, req *http.Request) {
+	ip := req.FormValue("ip")
+	conn, _, err := createConnect(ip)
+	if conn != nil { //连接失败时为nil，不能close
+		defer conn.Close()
+	}
 	if err != nil {
-		log.Println(err)
+		log.Printf("connect failed: %v", err)
+		log.Printf("connect failed:" + ip)
 	}
-	c := sc.NewSpacecraftClient(conn)
 
-	ct := reflect.TypeOf(c)
-	cc := reflect.ValueOf(c)
-	fmt.Println(cc, cc.NumMethod(), cc.Method(0))
-	fmt.Println(ct, ct.Method(0)) //method
-	fmt.Println(ct.Method(0).Name)
-	fmt.Println(ct.Method(0).PkgPath)
-	fmt.Println(ct.Method(0).Type) //type
-	fmt.Println(ct.Method(0).Func)
-	fmt.Println(ct.Method(0).Index)
-	pt := ct.Method(0).Type.In(2)
-	fmt.Println(ct.Method(0).Type.In(2).String()) //参数的类型
-	fmt.Println(reflect.ValueOf(reflect.Indirect(cc).Type().Name()).Type().Name())
-	fmt.Println(reflect.Indirect(cc).Type().Name())
+	clientObj := sc.NewSpacecraftClient(conn)
+	//ct := reflect.TypeOf(clientObj)
+	cc := reflect.ValueOf(clientObj)
 
-	//apiList := getApis(c)
-	//fmt.Println(apiList)
+	apiList := getApis(clientObj)
+	fmt.Println(apiList)
 	fmt.Println("=============")
-	tt := ct.Method(0).Type.In(2)
-	fmt.Println(reflect.TypeOf(tt).Elem().Kind())
-	fmt.Println(reflect.ValueOf(tt).Type().Kind())
-	fmt.Println(reflect.ValueOf(tt).Elem().Kind())
-	fmt.Println(reflect.ValueOf(tt).Elem().NumField())
-	v := reflect.Indirect(reflect.ValueOf(tt))
-	fmt.Printf("%#v", v)
-	news := reflect.New(reflect.TypeOf(tt))
-	fmt.Println(reflect.ValueOf(news).Type().NumField())
-	fmt.Println(reflect.TypeOf(news).Field(0).Tag.Get("json"))
-
-	//pa:=&interface{
-	//	Dir:"/path",
-	//	Command:"ps"}
-	//var p map[string]string{}
-	params := make(map[string]string)
-	params["Dir"] = "/a/b/c"
-	params["Command"] = "ps"
-	fmt.Println(params)
-	var sss interface{}
-	fmt.Println(sss.(pt))
-	for i, n := range params {
-		fmt.Println(i, n)
-		&sss{i: n}
+	tt := cc.MethodByName(req.FormValue("apiName"))
+	fmt.Println(tt.IsValid())
+	paramsStruct := getStruct(req.FormValue("apiName"))
+	fmt.Sprintf("%#v", paramsStruct)
+	paramsStruct = fillStruct(paramsStruct, req)
+	params := []interface{}{context.Background(), paramsStruct}
+	if tt.IsValid() {
+		args := make([]reflect.Value, len(params))
+		for k, param := range params {
+			args[k] = reflect.ValueOf(param)
+		}
+		// 调用
+		ret := tt.Call(args)
+		fmt.Println(ret, ret[0].Kind(), ret[0].String(), ret[0].Elem().FieldByName("String_").String())
+		if ret[0].Kind() == reflect.String {
+			fmt.Printf("%s Called result: %s\n", "方法名", ret[0].String())
+		}
+		w.Write([]byte(ret[0].Elem().FieldByName("String_").String()))
+	} else {
+		fmt.Println("can't call ")
 	}
-	//map[string]string{
-	//	"Dir":"/a/b/c",
-	//	"Command":"ps"
-	//	}
 
-	//强制提取字符串，在new出参数结构体，在赋值
+}
+
+func createConnect(ip string) (*grpc.ClientConn, sc.SpacecraftClient, error) {
+	conn, err := grpc.Dial(ip, grpc.WithInsecure(), grpc.WithTimeout(time.Second*5), grpc.WithBlock())
+	//defer conn.Close()
+	c := sc.NewSpacecraftClient(conn)
+	return conn, c, err
 }
 
 func getApis(object sc.SpacecraftClient) []string {
-	var re = []string{}
+	var apiList = []string{}
 	ct := reflect.TypeOf(object)
 	for i := 0; i < ct.NumMethod(); i++ {
-		//fmt.Println(ct.Method(i).Name)
-		re = append(re, ct.Method(i).Name)
+		apiList = append(apiList, ct.Method(i).Name)
+	}
+	return apiList
+}
+
+func fillStruct(s interface{}, req *http.Request) interface{} {
+	//s := &sa{}
+	//	s.Aaa = "aaa"
+	for i := 0; i < reflect.ValueOf(s).Elem().NumField(); i++ {
+		fieldName := reflect.TypeOf(s).Elem().Field(i).Name
+		fieldValue := reflect.ValueOf(s).Elem().FieldByName(fieldName)
+		valueType := fieldValue.Type().Kind().String()
+		fmt.Println(fieldName, fieldValue, fieldValue.Type().Kind())
+		//		fmt.Println(reflect.ValueOf(s).FieldByName(fieldName))
+		if !fieldValue.CanSet() {
+			log.Println("不可设置值，struct对象field不可设置")
+		}
+		if req.FormValue(fieldName) != "" {
+			tmp := req.FormValue(fieldName)
+			value, err := TypeConversion(tmp, valueType)
+			if err != nil {
+				log.Println(err)
+			}
+			fieldValue2 := value
+			fieldValue.Set(fieldValue2)
+		}
+	}
+	fmt.Sprintf("%#v", s)
+	return s
+}
+func getStruct(apiName string) interface{} {
+	switch apiName {
+	case "ComplexCommand":
+		return &sc.SpecifiedCommandParams{}
+	case "GetFileList":
+		return &sc.SvnUpParam{}
+	case "Live":
+		return &sc.Empty{}
+	case "SendFile":
+		return &sc.SendFileParams{}
+	case "SpecifiedCommand":
+		return &sc.SpecifiedCommandParams{}
+	case "SvnCheckout":
+		return &sc.SvnCheckoutParams{}
+	case "SvnInfo":
+		return &sc.SvnUpParam{}
+	case "SvnUp":
+		return &sc.SvnUpParam{}
+	case "SvnUpToRevision":
+		return &sc.SvnUpToRevisionParams{}
+	default:
+		return nil
+	}
+	return nil
+}
+
+//类型转换
+func TypeConversion(value string, ntype string) (reflect.Value, error) {
+	if ntype == "string" {
+		return reflect.ValueOf(value), nil
+	} else if ntype == "time.Time" {
+		t, err := time.ParseInLocation("2006-01-02 15:04:05", value, time.Local)
+		return reflect.ValueOf(t), err
+	} else if ntype == "Time" {
+		t, err := time.ParseInLocation("2006-01-02 15:04:05", value, time.Local)
+		return reflect.ValueOf(t), err
+	} else if ntype == "int" {
+		i, err := strconv.Atoi(value)
+		return reflect.ValueOf(i), err
+	} else if ntype == "int8" {
+		i, err := strconv.ParseInt(value, 10, 64)
+		return reflect.ValueOf(int8(i)), err
+	} else if ntype == "int32" {
+		i, err := strconv.ParseInt(value, 10, 64)
+		return reflect.ValueOf(int64(i)), err
+	} else if ntype == "int64" {
+		i, err := strconv.ParseInt(value, 10, 64)
+		return reflect.ValueOf(i), err
+	} else if ntype == "float32" {
+		i, err := strconv.ParseFloat(value, 64)
+		return reflect.ValueOf(float32(i)), err
+	} else if ntype == "float64" {
+		i, err := strconv.ParseFloat(value, 64)
+		return reflect.ValueOf(i), err
 	}
 
-	return re
+	//else if .......增加其他一些类型的转换
+
+	return reflect.ValueOf(value), errors.New("未知的类型：" + ntype)
 }
