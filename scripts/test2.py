34e0f6668e8371a21333f06860682009297a9bb0
wulinlw@gmail.com <wulinlw@gmail.com>
2016-06-22 11:26:30 +0800

    edit

--- scripts/test2.py
+++ scripts/test2.py
@@ -1,9 +1,2 @@
 #!/usr/bin/env python
-import os
-import sys
-
-dir_name = sys.argv[1]
-if not os.path.isdir(dir_name):
-    os.makedirs(dir_name)
-else:
-    print "directory already exists "
+print "test2 ok"
